ab9ab9ea5ebb47a4f95779183c6bc097d77a9c40
PhamLien2023 <149298581+PhamLien2023@users.noreply.github.com>
2023-10-30 07:59:49 +0800

    Update practice numbers and strings .py

--- practice numbers and strings .py
+++ practice numbers and strings .py
@@ -1,2 +1,2 @@
-print (" the cost operates one server per day:$ {:.2f}.".format (0.51* 24))
-print ( ' The cost operates one server per year: $ {:.2f}. '.format (0.51*24*365))
+print (" The cost to operate one server per day:$ {:.2f}.".format (0.51* 24))
+print ('The cost to operate one server per year: $ {:.2f}. '.format (0.51*24*365))
